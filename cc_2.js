8bc7edc1fd74ffb30ab1aa598123f49a35a0446d
Rachael <rachy.torok@gmail.com>
2025-01-25 21:35:05 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,13 @@ let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
 products.push("Tablet"); // Add a new product
 products.pop(); // Remove the last product
 console.log("Updated Product list: ", products);
+
+// Task 2 - Student Scores
+let scores = [88, 94, 78, 99, 80];
+scores[1] =90; // Update the second score
+// Calculate the average score
+let total = scores.reduce((sum, score) => sum + score, 0);
+let average = total / scores.length;
+// Display the updated scores and average
+console.log("Updated scores: ", scores);
+console.log("Average score: ", average);
