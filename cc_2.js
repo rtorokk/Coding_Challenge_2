5b6ee5011d300598a6fcc09b9bfc66e40826c528
Rachael <rachy.torok@gmail.com>
2025-01-25 21:31:08 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+// Task 1 - Store Inventory
+let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
+products.push("Tablet"); // Add a new product
+products.pop(); // Remove the last product
+console.log("Updated Product list: ", products);
